65ca03ff310895cba53ff386abdc6a3eeb986d71
EE-21104068-ArshAttri <97748634+EE-21104068-ArshAttri@users.noreply.github.com>
2022-01-14 22:49:12 +0530

    Python Assingment 1
    Arsh Attri
    21104068
    Electrical Engineering

---
+++ ZeroResult.py
@@ -0,0 +1,40 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================================================================== RESTART: /Users/ms/Downloads/untitled0.py ==================================================================
+Enter the value of first number
+7
+Enter the vale of second number
+9
+Enter the value of third number
+5
+The average of these three numbers is 7.0
+Enter the no. of dependents
+2
+Enter the gross income
+280000
+Your net Income Tax is: 52800.0
+Enter your SID
+21104068
+Enter your name
+Aerdt
+Enter ypur gennder, Use Gender values: 'F' , 'M' , 'U'(For Unknown)
+m
+Enter your course name
+y
+Enter your CGPA
+10
+[21104068, 'Aerdt', 'm', 10.0]
+Enter marks of first students
+10
+Enter the marks of second student
+20
+Enter the marks of third student
+30
+Enter the marks of fourth student
+40
+Enter the marks of fifth student
+50
+[10, 20, 30, 40, 50]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
