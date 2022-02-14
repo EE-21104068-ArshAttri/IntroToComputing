582d147f44be3a80df5753219a1adc650ceb2a89
EE-21104068-ArshAttri <97748634+EE-21104068-ArshAttri@users.noreply.github.com>
2022-02-14 23:24:05 +0530

    Add files via upload

---
+++ OUTPUT PythonA3.py
@@ -0,0 +1,108 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Alisha Attri
+#
+# Created:     14-02-2022
+# Copyright:   (c) Alisha Attri 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+Question: 1
+Enter The String: Arsh
+The Occurence Of:
+('a', 1)
+('r', 1)
+('s', 1)
+('h', 1)
+
+--------------------------------------------------------------------------------
+
+Question: 2
+Enter The Day: 26
+Enter The Month: 1
+Enter the year: 2022
+The Next Date is :  27/1/2022
+
+--------------------------------------------------------------------------------
+
+Question : 3
+Enter the list of numbers(Also add space between 2 numbers): 2 7 22 25
+[(2, 4), (7, 49), (22, 484), (25, 625)]
+--------------------------------------------------------------------------------
+
+Question 4
+Enter the Grade:- 10
+Your Grade is 'A+' and Outstanding Performace
+--------------------------------------------------------------------------------
+
+Question 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+--------------------------------------------------------------------------------
+
+Question 6
+Enter the Name of the Student:- Arsh Attri
+Enter the SID of the Student:- 21104068
+Type 'Y' if want enter another Student's Information or Type 'N' if don't want to Enter:- Y
+Enter the Name of the Student:- Pratyush
+Enter the SID of the Student:- 21104043
+Type 'Y' if want enter another Student's Information or Type 'N' if don't want to Enter:- N
+{21104068: 'Arsh Attri', 21104043: 'Pratyush'}
+{21104068: 'Arsh Attri', 21104043: 'Pratyush'}
+{21104043: 'Pratyush', 21104068: 'Arsh Attri'}
+Enter the SID to find the name:- 21104068
+Arsh Attri
+
+
+--------------------------------------------------------------------------------
+
+Question 7
+Enter the number of fibonacci terms you want:-
+8
+0
+1
+1
+2
+3
+5
+8
+13
+The Average of the numbers is:- 4.125
+
+
+--------------------------------------------------------------------------------
+
+Question 8
+Set1 =  {1, 2, 3, 4, 5}
+Set2 =  {8, 2, 4, 6}
+Set3 =  {1, 17, 5, 9, 13}
+a.)
+
+{1, 3, 5, 6, 8}
+
+b.)
+
+{3, 6, 8, 9, 13, 17}
+
+c.)
+
+{1, 2, 4, 5}
+
+d.)
+
+{6, 7, 8, 9, 10}
+
+e.)
+
+{10, 7}
+
+
+--------------------------------------------------------------------------------
+
+>>>
