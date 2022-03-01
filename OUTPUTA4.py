3283f92976fd9eae753f986944c4b25425a72661
EE-21104068-ArshAttri <97748634+EE-21104068-ArshAttri@users.noreply.github.com>
2022-03-01 23:55:38 +0530

    Add files via upload
    Name: Arsh Attri
    SID: 21104068
    Electrical Engineering

---
+++ OUTPUTA4.py
@@ -0,0 +1,131 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Alisha Attri
+#
+# Created:     01-03-2022
+# Copyright:   (c) Alisha Attri 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+TOWER OF HANOI
+
+Enter the rod from which to start[A/B/C]:A
+Enter the rod to which the disks need to be moved[A/B/C]:C
+
+Solution of the puzzle:
+Move the 1st disk : from A rod --> C rod.
+Move the 2nd disk : from A rod --> B rod.
+Move the 1st disk : from C rod --> B rod.
+Move the 3rd disk : from A rod --> C rod.
+Move the 1st disk : from B rod --> A rod.
+Move the 2nd disk : from B rod --> C rod.
+Move the 1st disk : from A rod --> C rod.
+--------------------------------------------------------------------------------
+
+PASCAL's TRIANGLE
+
+
+Recursive Procedure
+
+Enter the number of rows you want:7
+                    1
+                  1   1
+                1   2   1
+              1   3   3   1
+            1   4   6   4   1
+         1   5   10   10   5   1
+      1   6   15   20   15   6   1
+
+Iterative Procedure
+
+Enter the number of rows you want:4
+        1
+      1   1
+    1   2   1
+  1   3   3   1
+--------------------------------------------------------------------------------
+
+QUOTIENT AND REMAINDER
+
+Enter the first integer:15
+Enter the second integer:4
+
+Quotient : 3
+Remainder : 3
+
+Part a
+
+Checking if the function is callable : It's callable
+
+Part b
+
+All the values are non-zero.
+
+Part c
+
+Filtered out values which are greater than 4 : [5, 6]
+
+Part d
+
+Converted Set : {5, 6}
+
+Part e
+
+Immutable Set : frozenset({5, 6})
+
+Part f
+
+Maximum value : 6
+Hash value of max value : 6
+--------------------------------------------------------------------------------
+
+__init__ AND __del__ FUNCTIONS
+
+Name =VAIBHAV GUPTA
+Roll_no = 21104073
+--------------------------------------------------------------------------------
+
+EMPLOYEE DETAILS MANAGER
+
+
+  Details of all the Employees:
+
+Name		Salary
+-*-*-*-*-*-*-*-*-*-*-*-*
+Mehak		40000
+Ashok		5000
+Ashok		60000
+_________________________
+
+Part a : updating records
+
+
+  Details of all the Employees:
+
+Name		Salary
+-*-*-*-*-*-*-*-*-*-*-*-*
+Mehak		70000
+Ashok		5000
+Ashok		60000
+_________________________
+
+Part b : Deleting Records
+
+
+  Details of all the Employees:
+
+Name	Salary
+-*-*-*-*-*-*-*-*-*-*-*-*
+Mehak		70000
+Ashok		5000
+--------------------------------------------------------------------------------
+
+FRIENDSHIP TRUTHFULNESS TESTER
+
+Enter the word entered by George:abc
+Enter the word entered by Barbie:bcd
+
+Result:
+Their Friendship is Fake.
